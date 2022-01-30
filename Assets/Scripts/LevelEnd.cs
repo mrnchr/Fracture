8ed733f9de9aed721d1fc71cd778dc4ca70fa133
Subbota Mikhail <mihail.subbota@inbox.ru>
2022-01-30 14:44:23 +0300

    Added level end for altar scene

--- Assets/Scripts/LevelEnd.cs
+++ Assets/Scripts/LevelEnd.cs
@@ -5,11 +5,23 @@ using UnityEngine.SceneManagement;
 
 public class LevelEnd : MonoBehaviour
 {
+    private int _nextScene;
     private void OnTriggerEnter(Collider other)
     {
+        _nextScene = SceneManager.GetActiveScene().buildIndex + 1;
         if (other.tag == "Player")
         {
-            SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+            if (SceneManager.GetActiveScene().name == "Altar")
+                ActiveWin();
+            else
+                SceneManager.LoadScene(_nextScene);
         }
+    }
+
+    private void ActiveWin()
+    {
+        GameObject.FindGameObjectWithTag("Player").GetComponent<UnityStandardAssets.Characters.FirstPerson.FirstPersonController>().enabled = false;
+        //TODO: enable WinMenu
+
     }
 }
